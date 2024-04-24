8bfb50ac60537f0d4aa3721d5535c959701df4f0
Anton <antonio.fiasco24@gmail.com>
2024-04-24 12:50:40 +0100

    feat: profile pictures

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,18 +1,60 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flash_cards/widgets/back_button.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   ProfilePage({super.key});
 
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
   final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  String? imageUrl;
 
   Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
     return await FirebaseFirestore.instance
         .collection("Users")
         .doc(currentUser!.email)
         .get();
+  }
+
+  void pickUploadImage() async {
+    final image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxWidth: 512,
+      maxHeight: 512,
+      imageQuality: 75
+    );
+
+    Reference ref = FirebaseStorage.instance.ref().child("pfp-${currentUser!.email!}.jpg");
+
+    await ref.putFile(File(image!.path));
+    ref.getDownloadURL().then((value) {
+      print(value);
+      setState(() {
+        imageUrl = value;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    Reference ref = FirebaseStorage.instance.ref().child("pfp-${currentUser!.email!}.jpg");
+    ref.getDownloadURL().then((value) {
+      print(value);
+      setState(() {
+        imageUrl = value;
+      });
+    });
   }
 
   @override
@@ -47,16 +89,29 @@ class ProfilePage extends StatelessWidget {
                     ),
                   ),
                   const SizedBox(height: 25),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).colorScheme.primary,
-                      borderRadius: BorderRadius.circular(24)
+                  GestureDetector(
+                    onTap: pickUploadImage,
+                    child: imageUrl != null ?
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(24),
+                      child: Image.network(
+                        imageUrl!,
+                        height: 90,
+                        width: 90,
+                        fit: BoxFit.cover,
+                      ),
+                    ) :
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(24)
+                      ),
+                      padding: const EdgeInsets.all(13),
+                      child: const Icon(
+                        Icons.person,
+                        size: 64
+                      )
                     ),
-                    padding: const EdgeInsets.all(25),
-                    child: const Icon(
-                      Icons.person,
-                      size: 64
-                    )
                   ),
                   const SizedBox(height: 25),
                   Text(
