9e397c47642f7295545cc14edd4dd4bb3fcc3d9e
Anton <antonio.fiasco24@gmail.com>
2024-04-30 02:17:58 +0100

    added tests for local db

---
+++ test/local_database_test.dart
@@ -0,0 +1,48 @@
+import 'package:flash_cards/services/database_services.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite_common/sqlite_api.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+// Ref - https://stackoverflow.com/a/71136958
+// Ref - https://github.com/tekartik/sqflite/blob/master/sqflite/doc/testing.md
+
+/// Initialize sqflite for test.
+void sqfliteTestInit() {
+  // Initialize ffi implementation
+  sqfliteFfiInit();
+  // Set global factory
+  databaseFactory = databaseFactoryFfi;
+}
+
+void main() {
+  sqfliteTestInit();
+
+  test('DatabaseServices Functions Test', () async {
+    var db = await databaseFactory.openDatabase(inMemoryDatabasePath);
+    await _createDatabase(db, 1);
+
+    await DatabaseServices.saveTestResult('Test Set 1', 75.5);
+
+    List<Map<String, dynamic>> results = await DatabaseServices.getAllTestResults();
+
+    expect(results.length, 1);
+    expect(results[0]['cardsSetName'], 'Test Set 1');
+    expect(results[0]['percentage'], 75.5);
+
+    await DatabaseServices.saveTestResult('Test Set 2', 85.5);
+
+    int insertedId = results[0]['id'];
+    await DatabaseServices.deleteTestResult(insertedId);
+
+    List<Map<String, dynamic>> updatedResults = await DatabaseServices.getAllTestResults();
+    expect(updatedResults.length, 1);
+
+    await db.close();
+  });
+}
+
+Future<void> _createDatabase(Database database, int version) async {
+  const String sql =
+      'CREATE TABLE IF NOT EXISTS testResults(id INTEGER PRIMARY KEY AUTOINCREMENT, cardsSetName TEXT, percentage REAL)';
+  await database.execute(sql);
+}
---
+++ test/test_generator_test.dart
@@ -0,0 +1,69 @@
+import 'package:flash_cards/services/test_generator.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+List<dynamic> sampleTerms = [
+  {'id': "1", 'term': 'Term 1', 'definition': 'Definition 1'},
+  {'id': "2", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "3", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "4", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "5", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "6", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "7", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "8", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "9", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "10", 'term': 'Term 2', 'definition': 'Definition 2'},
+  {'id': "11", 'term': 'Term 2', 'definition': 'Definition 2'},
+  // Add more terms as needed
+];
+
+void main() {
+  test('Generated test should contain valid true/false items', () {
+
+    Map<String, dynamic> generatedTest = TestGenerator.generateTest(sampleTerms);
+
+    generatedTest['trueFalse'].forEach((item) {
+      expect(item.containsKey('id'), true);
+      expect(item['id'], isA<String>());
+
+      expect(item.containsKey('term'), true);
+      expect(item['term'], isA<String>());
+
+      expect(item.containsKey('definition'), true);
+      expect(item['definition'], isA<String>());
+
+      expect(item.containsKey('isTrue'), true);
+      expect(item['isTrue'], isA<bool>());
+
+      expect(item.containsKey('userAnswer'), true);
+      expect(item['userAnswer'], isNull);
+
+      expect(item.containsKey('incorrectAnswer'), true);
+      expect(item['incorrectAnswer'], anyOf(isNull, isA<Map<String, dynamic>>())); // incorrectAnswer can be null or string
+    });
+  });
+
+  test('Generated test should contain valid multiple choice items', () {
+
+    Map<String, dynamic> generatedTest = TestGenerator.generateTest(sampleTerms);
+
+    generatedTest['multipleChoice'].forEach((item) {
+      expect(item.containsKey('id'), true);
+      expect(item['id'], isA<String>());
+
+      expect(item.containsKey('term'), true);
+      expect(item['term'], isA<String>());
+
+      expect(item.containsKey('definition'), true);
+      expect(item['definition'], isA<String>());
+
+      expect(item.containsKey('possibleAnswers'), true);
+      expect(item['possibleAnswers'], isA<List<dynamic>>());
+
+      expect(item.containsKey('isCorrect'), true);
+      expect(item['isCorrect'], isA<bool>());
+
+      expect(item.containsKey('userAnswer'), true);
+      expect(item['userAnswer'], isNull);
+    });
+  });
+}
